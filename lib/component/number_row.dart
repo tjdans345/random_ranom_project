91ecd79e188df22c6e0b057aa92c79b616ffa330
meteor <tjdans34568@gmail.com>
2023-01-12 11:11:51 +0900

    코드 리팩토링 주석 추가2

--- lib/component/number_row.dart
+++ lib/component/number_row.dart
@@ -1,9 +1,8 @@
-
 // component 에는 여러 위젯에서 사용하는 중복되는 기능들을 따로 분리해서 빼놓으면 좋다
+// 중복되는 위젯을 컴포터넌트화(공통 위젯 추출) 하여 코드 중복을 지우고 유지보수가 더 수월하게 만들 수 있다.
 import 'package:flutter/material.dart';
 
 class NumberRow extends StatelessWidget {
-
   final int number;
 
   const NumberRow({required this.number, Key? key}) : super(key: key);
@@ -15,10 +14,10 @@ class NumberRow extends StatelessWidget {
           .toString()
           .split("")
           .map((number) => Image.asset(
-        "asset/img/$number.png",
-        width: 50.0,
-        height: 70.0,
-      ))
+                "asset/img/$number.png",
+                width: 50.0,
+                height: 70.0,
+              ))
           .toList(),
     );
   }
